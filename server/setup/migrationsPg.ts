ee58bd2871721420b92c14c25808682a928ac54d
Mauricio Allende <mauricio@palta.io>
2026-03-01 00:35:46 -0300

    chore: bump version to 1.17.0 and rename migration scripts

--- server/setup/migrationsPg.ts
+++ server/setup/migrationsPg.ts
@@ -21,7 +21,7 @@ import m12 from "./scriptsPg/1.15.0";
 import m13 from "./scriptsPg/1.15.3";
 import m14 from "./scriptsPg/1.15.4";
 import m15 from "./scriptsPg/1.16.0";
-import m16 from "./scriptsPg/1.16.1";
+import m16 from "./scriptsPg/1.17.0";
 
 // THIS CANNOT IMPORT ANYTHING FROM THE SERVER
 // EXCEPT FOR THE DATABASE AND THE SCHEMA
@@ -43,7 +43,7 @@ const migrations = [
     { version: "1.15.3", run: m13 },
     { version: "1.15.4", run: m14 },
     { version: "1.16.0", run: m15 },
-    { version: "1.16.1", run: m16 }
+    { version: "1.17.0", run: m16 }
     // Add new migrations here as they are created
 ] as {
     version: string;
