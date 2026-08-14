f6e7a3b7118782089a3ce7b8cb8961ffcab8d881
Shlee <github@shl.ee>
2026-08-14 10:39:52 +0930

    Update schema.ts

--- server/db/sqlite/schema/schema.ts
+++ server/db/sqlite/schema/schema.ts
@@ -211,6 +211,11 @@ export const resources = sqliteTable("resources", {
         .$type<"site" | "remote" | "native">()
         .default("site"),
     authDaemonPort: integer("authDaemonPort").default(22123),
+    enableCompress: integer("enableCompress", { mode: "boolean" }).notNull().default(false),
+    compressExcludedContentTypes: text("compressExcludedContentTypes"), // JSON array of strings
+    enableCache: integer("enableCache", { mode: "boolean" })
+        .notNull()
+        .default(false),
     status: text("status").$type<"pending" | "approved">().default("approved")
 });
 
