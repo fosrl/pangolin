c9ecee37fb12cc51dd9177abb65c6dc1551feb58
Shlee <github@shl.ee>
2026-08-14 12:17:33 +0930

    Update schema.ts

--- server/db/pg/schema/schema.ts
+++ server/db/pg/schema/schema.ts
@@ -179,7 +179,7 @@ export const resources = pgTable(
         enableCompress: boolean("enableCompress").notNull().default(false),
         compressExcludedContentTypes: text("compressExcludedContentTypes"), // JSON array of strings
         enableCache: boolean("enableCache").notNull().default(false),
-        enableCachePath: text("enableCachePath").notNull(),
+        enableCachePath: text("enableCachePath").notNull().default("/tmp"),
         skipToIdpId: integer("skipToIdpId").references(() => idp.idpId, {
             onDelete: "set null"
         }),
