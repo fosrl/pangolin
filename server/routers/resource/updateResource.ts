e9e8457d06ed3e9324f2ebb0ac2ba6db0635917e
Shlee <github@shl.ee>
2026-08-16 21:29:44 +0930

    Update updateResource.ts

--- server/routers/resource/updateResource.ts
+++ server/routers/resource/updateResource.ts
@@ -498,7 +498,8 @@ async function updateHttpResource(
     }
 
     // catch when the resource policy changes or gets cleared
-    if (resource.resourcePolicyId != updateData.resourcePolicyId) {
+    if (updateData.resourcePolicyId !== undefined && 
+        resource.resourcePolicyId !== updateData.resourcePolicyId) {
         await clearResourceSpecificSettings(
             resource.resourceId,
             resource.orgId,
